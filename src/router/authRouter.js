6ac5aea63a0eb8902caf7470a31892c247c593df
userFrontend <aba06096@gmail.com>
2025-05-13 18:38:35 +0500

    save

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -1,15 +1,14 @@
 const router = require('express').Router()
 const authCtrl = require('../controller/authCtrl')
-const authMiddleware = require('../middleware/authMiddleware')
 const chechAccessMiddleware = require('../middleware/checkAccess')
 
 // FormData register
 router.post('/mail' , chechAccessMiddleware, authCtrl.sendMail)
 router.get('/year/:year' , chechAccessMiddleware,  authCtrl.getYear)
 router.get('/make/:year/:make' , chechAccessMiddleware, authCtrl.getMakes)
-router.post('/login' , chechAccessMiddleware, authMiddleware, authCtrl.login)
-router.post('/signup', chechAccessMiddleware, authMiddleware, authCtrl.signup)
-router.post('/google' , chechAccessMiddleware, authMiddleware, authCtrl.googleAuth)
-router.post('/forgot' , chechAccessMiddleware, authMiddleware, authCtrl.forgotPassword)
+router.post('/login' , chechAccessMiddleware, authCtrl.login)
+router.post('/signup', chechAccessMiddleware, authCtrl.signup)
+router.post('/google' , chechAccessMiddleware, authCtrl.googleAuth)
+router.post('/forgot' , chechAccessMiddleware, authCtrl.forgotPassword)
 
 module.exports = router
